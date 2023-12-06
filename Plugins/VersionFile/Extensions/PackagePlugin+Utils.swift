b1f5cee4453f0c6e838d28b2ea8c25f0f9604407
Mathew Gacy <matt@mgacy.com>
2023-12-06 09:12:42 -0700

    Support Executable Targets (#2)
    * Refactor extractCommand()
    
    * Add documentation
    
    * Add documentation to the generated type
    
    * Add support for executable targets
    
    * Revert extractCommand changes
    
    * Improve readability of verbose message
    
    * More readability improvements

---
+++ Plugins/VersionFile/Extensions/PackagePlugin+Utils.swift
@@ -0,0 +1,23 @@
+//
+//  PackagePlugin+Utils.swift
+//  
+//
+//  Created by Mathew Gacy on 12/3/23.
+//
+
+import Foundation
+import PackagePlugin
+
+extension PackagePlugin.Target {
+    var debugDescription: String {
+        if let sourceModuleTarget = self as? SourceModuleTarget {
+            return """
+                SourceModuleTarget(id: "\(id)", name: "\(name)", moduleName: "\(sourceModuleTarget.moduleName)", kind: ModuleKind.\(sourceModuleTarget.kind))
+                """
+        } else {
+            return """
+                Target(id: "\(id)", name: "\(name)", directory: \(directory))
+                """
+        }
+    }
+}
